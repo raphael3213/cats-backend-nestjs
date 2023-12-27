6cb8bb1d3948acda5efc68d7967a760737b7ade5
joel <raphael3213@gmail.com>
2023-12-27 15:38:49 +0530

    Add tests to launch.json

--- src/utility/storage/storage.service.spec.ts
+++ src/utility/storage/storage.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { StorageService } from '../storage.service';
+import { StorageService } from './storage.service';
 
 describe('StorageService', () => {
   let service: StorageService;
